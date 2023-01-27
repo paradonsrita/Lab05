c98432ad6827b61a47c946d6e61c13706be4659a
paradonsrita <paradonsrita2003@gmail.com>
2023-01-27 22:21:07 +0700

    Lab05_1

--- src/components/LikeSad.vue
+++ src/components/LikeSad.vue
@@ -15,7 +15,7 @@ function sadClick(){
 
 <template>
     <div>
-      <h3>หัวเรื่อง</h3>
+      <h3>title</h3>
       <div class="col">
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
@@ -23,9 +23,12 @@ function sadClick(){
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <button @click="likeClick">Like</button>
+                  <button @click="likeClick"><i class="bi bi-star-fill"></i></button>
                   like = {{ like }}
-                  <button @click="sadClick">Sad</button>
+                  <button @click="sadClick"><i class="bi bi-emoji-frown-fill"></i></button>
+                  
+                    <p>. .</p>
+                    
                   sad = {{ sad }}
 
                 </div>
